7f11742e8ef446578912992d088ee195365af828
karthikbenki <karthikmc007@gmail.com>
2022-02-06 11:08:38 +0530

    Added: calculation of Employee Wages per month logic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -5,6 +5,7 @@ public class EmployeeWageComputation {
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
 	static final int EMP_RATE_PER_HR = 20;
+	static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static int empCheckStatus() {
 		int empWrkHrs = 0;
@@ -36,9 +37,15 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {	
 		int empWrkHrs;
 		int empWagePerDay;
-		empWrkHrs = empCheckStatus();
-		empWagePerDay = empWagePerDayStatus(empWrkHrs);
-		System.out.println("Employee Wage per day: "+empWagePerDay);
+		int days=0;
+		int empMonthWage = 0;		
+		while(days < NUM_OF_WORKING_DAYS) {
+			days++;
+			empWrkHrs = empCheckStatus();
+			empWagePerDay = empWagePerDayStatus(empWrkHrs);
+			empMonthWage += empWagePerDay;	
+		}
+		System.out.println("Employee Wage per month: "+empMonthWage);
 		
 	}
 
