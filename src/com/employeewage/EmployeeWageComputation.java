fd6cfac5ba06abdcb1f5d29d437f6a12c8729886
karthikbenki <karthikmc007@gmail.com>
2022-02-06 08:40:31 +0530

    Added: Employee is present or absent logic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -1,9 +1,19 @@
 package com.employeewage;
+import java.util.Random;
 
 public class EmployeeWageComputation {
+	static final int IS_FULL_TIME = 1;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+		
 
 	}
 
