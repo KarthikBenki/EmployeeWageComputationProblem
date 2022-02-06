c91c8f4241b39310c22caca0e09ecdbc21cbe7ab
karthikbenki <karthikmc007@gmail.com>
2022-02-06 09:01:55 +0530

    Added: Calculating Daily Employee Wage Logic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -3,20 +3,35 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 	static final int IS_FULL_TIME = 1;
+	static final int EMP_RATE_PER_HR = 20;
 	
-	public static void empCheckStatus() {
+	public static int empCheckStatus() {
+		int empWrkHrs = 0;
 		Random random = new Random();
 		int empCheck = random.nextInt(2);
 		if(empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
+			empWrkHrs = 8;
 		}
 		else {
 			System.out.println("Employee is absent");
+			empWrkHrs = 0;
 		}
+		return empWrkHrs;
+	}
+	
+	public static int empWagePerDayStatus(int empWrkHrs) {
+		int empWagePerDay = empWrkHrs * EMP_RATE_PER_HR;
+		return empWagePerDay;
 	}
 
-	public static void main(String[] args) {		
-		empCheckStatus();				
+	public static void main(String[] args) {	
+		int empWrkHrs;
+		int empWagePerDay;
+		empWrkHrs = empCheckStatus();
+		empWagePerDay = empWagePerDayStatus(empWrkHrs);
+		System.out.println("Employee Wage per day: "+empWagePerDay);
+		
 	}
 
 }
